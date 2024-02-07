e85932f5d478a474c0e73251315d2690f568425d
EgorLum <grggvzd@gmail.com>
2024-02-07 09:26:00 +0300

    Initial commit

---
+++ spring_boot3.1.2/src/main/java/spring_boot/web/spring_boot312/Application.java
@@ -0,0 +1,13 @@
+package spring_boot.web.spring_boot312;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
